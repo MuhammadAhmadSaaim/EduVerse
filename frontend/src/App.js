f4cf280ecf3c90c59078f1bf83b581a4213db999
Ahmad <131032658+MuhammadAhmadSaaim@users.noreply.github.com>
2024-11-18 18:40:07 +0500

    Frontend Setup

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div className="bg-primary p-6 text-center">
+        <h1 className="text-2xl font-bold text-red-600">Welcome to My App</h1>
+        <p className="mt-4">This is an example component styled with Tailwind CSS.</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Adjust the path to match where your files are located
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
